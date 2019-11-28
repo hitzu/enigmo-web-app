c788e42a5223c80be435893b59a107d842889e09
hitzu <robert.torres.lopez@gmail.com>
2019-11-28 11:53:10 -0600

    [update] (graffiti) se agrego la primera version del graffiti selected

--- src/components/mapComponents/graffitiMarker/graffitiState/graffitiPreviewComponents/ImageGraffitiPreview.component.jsx
+++ src/components/mapComponents/graffitiMarker/graffitiState/graffitiPreviewComponents/ImageGraffitiPreview.component.jsx
@@ -4,13 +4,12 @@ import PropTypes from 'prop-types'
 class ImageGraffitiPreview extends React.Component {
 
     static propTypes = { 
-        graffitiDescription : PropTypes.string.isRequired,
         graffitiImage : PropTypes.string.isRequired
     }
 
     render(){
 
-        const { graffitiDescription, graffitiImage } = this.props
+        const { graffitiImage } = this.props
 
         console.log(graffitiImage)
         return(
@@ -19,7 +18,6 @@ class ImageGraffitiPreview extends React.Component {
                 width:'100%',
                 backgroundColor: 'green'
             }}> 
-                <h1>{ graffitiDescription }</h1>
                 <img src = { graffitiImage } />
             </div>
         )
